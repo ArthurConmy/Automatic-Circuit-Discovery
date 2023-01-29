5204d175fa60702642f4dea593667f88ec02109f
ArthurConmy <arthurconmy@gmail.com>
2023-01-28 23:07:54 -0800

    com

---
+++ circuit_discovery.py
@@ -0,0 +1,6 @@
+#%%
+
+# see https://www.lyndonduong.com/saving-activations/
+# probably use the partial trick in order to give all hooks access to the model
+
+# also probably being hierarchical is best...
