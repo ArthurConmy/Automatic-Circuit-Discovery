f230dc6672f1c0a101fede5cf554e05cc5249fdf
Arthur Conmy <arthurconmy@gmail.com>
2023-03-25 19:12:41 +0000

    More progress

--- transformer_lens/acdc.py
+++ transformer_lens/acdc.py
@@ -12,13 +12,16 @@ import IPython
 import torch
 # from easy_transformer.ioi_dataset import IOIDataset  # type: ignore
 from tqdm import tqdm
-
+import random
 from functools import *
 import json
 import pathlib
+import warnings
 import time
+import networkx as nx
 import os
 import torch
+import graphviz
 from enum import Enum
 import torch.nn as nn
 import torch.nn.functional as F
@@ -41,7 +44,7 @@ from transformer_lens.HookedTransformer import (
 )
 from transformer_lens.utils import make_nd_dict, TorchIndex # these introduce several important classes !!!
 from collections import defaultdict, deque, OrderedDict
-from transformer_lens.induction.utils import kl_divergence, toks_int_values, toks_int_values_other, good_induction_candidates, validation_data
+from transformer_lens.induction.utils import kl_divergence, toks_int_values, toks_int_values_other, good_induction_candidates, validation_data, build_colorscheme, show
 
 #rust%%
 
@@ -65,7 +68,7 @@ metric = partial(kl_divergence, base_model_probs=base_model_probs)
 
 #%%
 
-full_graph = make_nd_dict(end_type=list, n=3) # TODO really global variable
+full_graph = make_nd_dict(end_type=bool, n=4) # TODO really global variable
 all_senders_names = set() # get receivers by .keys()
 
 residual_stream_items: Dict[str, List[TorchIndex]] = defaultdict(list)
@@ -77,7 +80,7 @@ def add_edge(
     sender_name: str,
     sender_slice: TorchIndex,
 ):
-    full_graph[receiver_name][receiver_slice][sender_name].append(sender_slice)
+    full_graph[receiver_name][receiver_slice][sender_name][sender_slice] = True
     all_senders_names.add(sender_name)
 
 for layer_idx in range(tl_model.cfg.n_layers-1, -1, -1):
@@ -108,33 +111,38 @@ for receiver_name in residual_stream_items:
 
 # %%
 
-current_graph = deepcopy(full_graph)
-nodes = OrderedDict() # used as an OrderedSet replacement
-for node_name in full_graph.keys():
-    for node_slice in full_graph[node_name].keys():
-        nodes[(node_name, node_slice)] = None
-
-# %%
-
-# TODO do the corrupt caching
-
-corrupt_cache_hooks = []
-
-def corrupt_cache_hook(z, hook):
-    pass
+if False:
+    current_graph = deepcopy(full_graph)
+    nodes = OrderedDict() # used as an OrderedSet replacement
+    for node_name in full_graph.keys():
+        for node_slice in full_graph[node_name].keys():
+            nodes[(node_name, node_slice)] = None
 
 #%%
 
 # add the saving hooks
 
-def sender_hook(z, hook):
-    hook.global_cache.cache[hook.name] = z.clone()
-    print(hook.name, z.norm().item())
+def sender_hook(z, hook, verbose=False, cache="first"):
+    """General, to cover online and corrupt caching"""
+
+    if cache == "second":
+        hook.global_cache.second_cache[hook.name] = z.clone()
+    elif cache == "first":
+        hook.global_cache.cache[hook.name] = z.clone()
+    else:
+        raise ValueError(f"Unknown cache type {cache}")
+
+    if verbose:
+        print(f"Saved {hook.name} with norm {z.norm().item()}")
+
     return z
 
 tl_model.reset_hooks()
 for name in all_senders_names:
-    tl_model.add_hook(name=name, hook=sender_hook)
+    tl_model.add_hook(name=name, hook=partial(sender_hook, verbose=False, cache="second"))
+
+corrupt_stuff = tl_model(toks_int_values_other)
+tl_model.reset_hooks()
 
 #%%
 
@@ -144,9 +152,14 @@ def receiver_hook(z, hook):
     for receiver_slice_tuple in full_graph[hook.name].keys():
         for sender_name in full_graph[hook.name][receiver_slice_tuple].keys():
             for sender_slice_tuple in full_graph[hook.name][receiver_slice_tuple][sender_name]:
+                if full_graph[hook.name][receiver_slice_tuple][sender_name][sender_slice_tuple]:
+                    # do "actually" include this edge
+                    continue
+
                 print(hook.name, receiver_slice_tuple, sender_name, sender_slice_tuple)
                 print(z.norm().item())
                 print(receiver_slice_tuple.as_index, sender_slice_tuple.as_index)
+
                 z[receiver_slice_tuple.as_index]
                 hook.global_cache.cache[sender_name][sender_slice_tuple.as_index]
                 z[receiver_slice_tuple.as_index] -= hook.global_cache.cache[sender_name][sender_slice_tuple.as_index]
@@ -154,6 +167,12 @@ def receiver_hook(z, hook):
     # assert torch.allclose(z, torch.zeros_like(z)) or torch.allclose(z, b_O)
     return z
 
+# add both sender and receiver hooks
+for receiver_name in full_graph.keys():
+    tl_model.add_hook(
+        name=receiver_name,
+        hook=partial(sender_hook, cache="first"),
+    )
 for sender_name in full_graph.keys():
     tl_model.add_hook(
         name=sender_name,
@@ -162,7 +181,141 @@ for sender_name in full_graph.keys():
 
 #%%
 
-b = torch.arange(5) # note that biases mean not EVERYTHING is zero ablated
+ans = tl_model(toks_int_values) # torch.arange(5) # note that biases mean not EVERYTHING is zero ablated
+new_metric = metric(ans)
+assert abs(new_metric) < 1e-5, f"Metric {new_metric} is not zero"
+
+
+#%%
+
+def vizualize_graph(graph):
+    G = nx.DiGraph()
+    for receiver_name in graph.keys():
+        for receiver_slice_tuple in graph[receiver_name].keys():
+            for sender_name in graph[receiver_name][receiver_slice_tuple].keys():
+                for sender_slice_tuple in graph[receiver_name][receiver_slice_tuple][sender_name]:
+                    G.add_edge(sender_name, receiver_name, weight=1 + int(graph[receiver_name][receiver_slice_tuple][sender_name][sender_slice_tuple]))
+    return G
+
+graph = vizualize_graph(full_graph)
+
+#%%
+
+show(graph, "test.png")
+
+#%%
+
+def step(
+    graph,
+    threshold,
+    receiver_name,
+    receiver_slice_tuple,
+    verbose=True,
+    using_wandb=False,
+):
+    """
+    TOIMPLEMENT:
+    - Incoming effect sizes on the nodes
+    - Dynamic threshold?
+    - Node stats
+    - Parallelism
+    - Not just monotone metric
+    - wandb
+    """
+    warnings.warn("Implement incoming effect sizes on the nodes")
+    start_step_time = time.time()
+
+    initial_metric = metric(tl_model(toks_int_values))
+    assert isinstance(initial_metric, float), "Make this float"
+    cur_metric = initial_metric
+    if verbose:
+        print("New metric:", cur_metric)
+
+    for sender_name in graph[receiver_name][receiver_slice_tuple]:
+        print(f"\nNode name: {sender_name}\n")
+
+        for sender_slice_tuple in graph[receiver_name][receiver_slice_tuple][sender_name]:
+            graph[receiver_name][receiver_slice_tuple][sender_name][sender_slice_tuple] = False
+
+            evaluated_metric = metric(tl_model(toks_int_values))
+
+            if verbose:
+                print(
+                    "Metric after removing connection to",
+                    sender_name,
+                    sender_slice_tuple,
+                    "is",
+                    evaluated_metric,
+                    "(and current metric " + str(cur_metric) + ")",
+                )
+
+            result = evaluated_metric - cur_metric
+
+            if verbose:
+                print("Result is", result, end="")
+
+            if result < threshold:
+                print("...so removing connection")
+                cur_metric = evaluated_metric
+
+            else:
+                if verbose:
+                    print("...so keeping connection")
+                graph[receiver_name][receiver_slice_tuple][sender_name][sender_slice_tuple] = True
+
+            if using_wandb:
+                raise NotImplementedError("Wandb not implemented yet")
+    
+    # TODOTODOTODO Arthur implement this
+    if len(self.current_node.children) == 0:
+        print(
+            f"Warning: we added {self.current_node.name} earlier, but we just removed all its child connections. So we are{(' ' if self.remove_redundant else ' not ')}removing it and its redundant descendants now (remove_redundant={self.remove_redundant})"
+        )
+        self.current_node.is_redundant = True
+        if self.remove_redundant:
+            queue = [self.current_node]
+            idx = 0
+            for idx in range(0, int(1e9)):
+                if idx >= len(queue):
+                    break
+                parents = list(queue[idx].parents)
+                for parent in parents:
+                    self.remove_connection(
+                        parent_node=typing.cast(ACDCInterpNode, parent),
+                        child_node=typing.cast(ACDCInterpNode, queue[idx]),
+                        suppress_warning=True,
+                    )
+                    if len(parent.children) == 0 and parent not in queue:
+                        parent.is_redundant = True
+                        queue.append(parent)
+    # TODO: this is annoying
+    if self.using_wandb:
+        fname = "acdc_plot_" + str(self.current_node.name) + ".png"
+        if self.wandb_project_name is not None:
+            fname = self.wandb_project_name + "/current_run/" + fname
+        self._nodes.show(fname=fname, show=False)
+        self.log_metrics_to_wandb(picture_fname=fname)
+
+    self.increment_current_node()
+    end_step_time = time.time()
+    step_time = end_step_time - start_step_time
+    
+    # sad, wish this could be logged as a system metric...
+    if self.using_wandb:
+        wandb.log({"step_time": step_time})
+
+#%%
+
+# Let's do some ACDC !!!
+
+for receiver_name in full_graph.keys():
+    for receiver_slice_tuple in full_graph[receiver_name]:
+
+        print("Currently at ")
+
+        for sender_name in full_graph[receiver_name][receiver_slice_tuple]:
+            for sender_slice_tuple in full_graph[receiver_name][receiver_slice_tuple][sender_name]:
+                full_graph[receiver_name][receiver_slice_tuple][sender_name][sender_slice_tuple] = True
 
 #%%
 
@@ -192,4 +345,3 @@ for step_idx in range(int(1e6)):
             # try and remove this
             full_graph[node_name][node_slice][sender_name].remove(sender_slice)
 
-# %%
--- transformer_lens/induction/utils.py
+++ transformer_lens/induction/utils.py
@@ -3,6 +3,12 @@ import pickle
 import torch
 import huggingface_hub
 import datetime
+from typing import Dict
+import torch
+import random
+import torch.nn as nn
+import torch.nn.functional as F
+import warnings
 
 DEVICE = "cuda:0"
 SEQ_LEN = 300
@@ -23,12 +29,14 @@ PROJECT_NAME = f"induction_arthur"
 validation_fname = huggingface_hub.hf_hub_download(repo_id="ArthurConmy/redwood_attn_2l", filename="validation_data.pt")
 validation_data = torch.load(validation_fname)
 
-# good_induction_candidates_fname = huggingface_hub.hf_hub_download(repo_id="ArthurConmy/redwood_attn_2l", filename="good_induction_candidates.pt")
-# good_induction_candidates = torch.load(good_induction_candidates_fname)
+good_induction_candidates_fname = huggingface_hub.hf_hub_download(repo_id="ArthurConmy/redwood_attn_2l", filename="good_induction_candidates.pt")
+good_induction_candidates = torch.load(good_induction_candidates_fname)
 
 mask_repeat_candidates_fname = huggingface_hub.hf_hub_download(repo_id="ArthurConmy/redwood_attn_2l", filename="mask_repeat_candidates.pkl")
 mask_repeat_candidates = torch.load(mask_repeat_candidates_fname)
-  
+mask_repeat_candidates.requires_grad = False
+mask_repeat_candidates = mask_repeat_candidates[:NUM_EXAMPLES, :SEQ_LEN]
+
 def shuffle_tensor(tens):
     """Shuffle tensor along first dimension"""
     torch.random.manual_seed(42)
@@ -44,13 +52,68 @@ def kl_divergence(
     base_model_probs: torch.Tensor,
 ):
     """Compute KL divergence between base_model_probs and probs"""
-    labels = dataset.arrs["labels"].evaluate()
     probs = F.softmax(logits, dim=-1)
 
     assert probs.min() >= 0.0
     assert probs.max() <= 1.0
 
     kl_div = (base_model_probs * (base_model_probs.log() - probs.log())).sum(dim=-1)
-    kl_div = kl_div * good_induction_candidates_reshaped.long()
 
-    return (kl_div.sum() / denom).item()
+    assert kl_div.shape == mask_repeat_candidates.shape, (kl_div.shape, mask_repeat_candidates.shape)
+    kl_div = kl_div * mask_repeat_candidates.long()
+
+    return (kl_div.sum() / mask_repeat_candidates.int().sum()).item()
+
+# -------------------------------------------
+# SOME GRAPHICS 
+# -------------------------------------------
+
+import graphviz
+
+def generate_random_color(colorscheme: str) -> str:
+    """
+    https://stackoverflow.com/questions/28999287/generate-random-colors-rgb
+    """
+    import cmapy
+
+    def rgb2hex(rgb):
+        """
+        https://stackoverflow.com/questions/3380726/converting-an-rgb-color-tuple-to-a-hexidecimal-string
+        """
+        return "#{:02x}{:02x}{:02x}".format(rgb[0], rgb[1], rgb[2])
+
+    return rgb2hex(cmapy.color("Pastel2", random.randrange(0, 256), rgb_order=True))
+
+def build_colorscheme(node_names, colorscheme: str = "Pastel2") -> Dict[str, str]:
+    colors = {}
+    for node in node_names:
+        colors[str(node)] = generate_random_color(colorscheme)
+    return colors
+
+def show(
+    graph,
+    fname: str,
+    colorscheme: str = "Pastel2",
+):
+    """
+    takes matplotlib colormaps
+    """
+    g = graphviz.Digraph(format="png")
+    colors = build_colorscheme(list(graph.nodes))
+    warnings.warn("This hardcodes in the allsendersnames")
+
+    for child in graph.nodes:
+        for parent in graph[child]:
+            penwidth = {1: 1, 2: 11}[graph[child][parent]["weight"]] # self.get_connection_strengths(parent, child, minimum_penwidth)
+            g.edge(
+                child,
+                parent,
+                penwidth=str(penwidth),
+                color=colors[child],
+            )
+
+    # if fname is not None:
+
+    assert fname.endswith(".png"), "Must save as png (... or you can take this g object and read the graphviz docs)"
+    g.render(outfile=fname, format="png")
+    return g
