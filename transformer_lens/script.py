8dc5cf47cd54ada572a63627093e6045c48ba5b1
Arthur Conmy <arthurconmy@gmail.com>
2023-03-27 15:28:02 +0000

    Add a more useful runner

--- transformer_lens/script.py
+++ transformer_lens/script.py
@@ -7,4 +7,4 @@ STOP = 1.5
 for it in range(3, int(1e6)):
     curspace = np.linspace(start=START, stop=STOP, num=it)
     for threshold in curspace[1:-1]:
-        subprocess.run(["python", "acdc.py", "--threshold", str(threshold)])
+        subprocess.run(["python", "/mnt/ssd-0/arthurworkspace/TransformerLens/transformer_lens/acdc.py", "--threshold", str(threshold)])
