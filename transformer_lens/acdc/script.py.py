64cc8403f68086a3923a4f0fb17fc04ab8507d68
Arthur Conmy <arthurconmy@gmail.com>
2023-04-02 00:39:33 +0000

    Stuff

---
+++ transformer_lens/acdc/script.py.py
@@ -0,0 +1,13 @@
+import subprocess
+import numpy as np 
+from math import gcd
+
+START = 0.0
+STOP = 2.0
+
+for it in range(3, int(1e6)):
+    curspace = np.linspace(start=START, stop=STOP, num=it)
+    for threshold_idx, threshold in list(enumerate(curspace))[1:-1]:
+        if gcd(threshold_idx, it) != 1:
+            continue
+        subprocess.run(["python", "acdc.py", "--config", "../../configs_acdc/base_config.yaml", "--threshold", str(threshold), "--zero-ablation"])
