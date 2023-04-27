cd702103ff1d840ea2734e360e6693ab870e7b19
Adrià Garriga-Alonso <adria.garriga@gmail.com>
2023-04-26 17:45:55 -0700

    1.2.6

--- experiments/launch_induction.py
+++ experiments/launch_induction.py
@@ -49,7 +49,7 @@ def main(testing=False, use_kubernetes=False):
                                 "run",
                                 f"--name=agarriga-acdc-{i:03d}",
                                 "--shared-host-dir-slow-tolerant",
-                                "--container=ghcr.io/rhaps0dy/automatic-circuit-discovery:1.2.5",
+                                "--container=ghcr.io/rhaps0dy/automatic-circuit-discovery:1.2.6",
                                 "--cpu=4",
                                 "--gpu=1",
                                 "--login",
