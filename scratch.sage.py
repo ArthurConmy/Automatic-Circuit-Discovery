fb44dc0344f465d2d1ab0326f833995ebdec2007
Arthur Conmy <arthurconmy@gmail.com>
2023-04-28 04:39:57 +0100

    Good stuff

--- scratch.sage.py
+++ scratch.sage.py
@@ -9,9 +9,16 @@ if get_ipython() is not None:
 from tqdm import tqdm
 import warnings
 import acdc
+from copy import deepcopy
 from acdc import HookedTransformer
 from acdc.induction.utils import get_all_induction_things
 import torch
+from acdc.acdc_utils import TorchIndex, Edge, EdgeType, OrderedDefaultdict, make_nd_dict
+
+RECOMPUTE = False
+
+if not RECOMPUTE:
+    torch.set_grad_enabled(False)
 
 # %%
 
@@ -55,7 +62,9 @@ clean_logits = tl_model(toks_int_values)
 kl_result = metric(clean_logits)
 assert list(kl_result.shape) == [num_examples, seq_len], kl_result.shape
 kl_result = (kl_result * mask_rep).sum() / mask_rep.int().sum().item()
-kl_result.backward(retain_graph=True)
+
+if RECOMPUTE:
+    kl_result.backward(retain_graph=True)
 
 # %%
 
@@ -105,6 +114,9 @@ for i in tqdm(range(num_examples)):
             for head_idx in range(8):
                 results[(layer_idx, head_idx)][(i, j)] = cur_results[i, head_idx].item()
 
+for k in results:
+    results[k].to("cpu")
+
 # %%
 
 kls = {
@@ -120,7 +132,6 @@ for i in tqdm(range(num_examples)):
             continue  # lolololol
 
         tl_model.zero_grad()
-        warnings.warn("Untested reset...")
         tl_model.reset_hooks()
         clean_cache = tl_model.add_caching_hooks(incl_bwd=True)
         clean_logits = tl_model(toks_int_values)
@@ -140,10 +151,17 @@ for i in tqdm(range(num_examples)):
                 )
                 kls[(layer_idx, head_idx)][i, j] = g
 
+for k in kls:
+    kls[k].to("cpu")
+
 # %%
 
 for k in results:
     print(k, results[k].norm().item(), kls[k].norm().item())  # should all be close!!!
+    assert torch.allclose(results[k], kls[k])
+
+import gc; gc.collect()
+torch.cuda.empty_cache()
 
 # note the shape of these is 40*300, to get expectation take the sum and divide by mask_reps.sum().int()
 
@@ -151,70 +169,138 @@ for k in results:
 
 # some intersection with Aug's works!
 # goal 1: get order of heads
-# goal 2:
+# goal 2: edit xis 
+# goal 3: compute num edges
 
 # %%
-def compute_scores(kl_dict):
-    scores_list = []
+
+# def compute_scores(kl_dict):
+kl_dict = deepcopy(results)
+if True:
+    scores_list = torch.zeros(size=(2, 8))
     mask_list = []
-    for key in kl_dict.keys():
-        score = kl_dict[key].sum() / mask_rep.sum().int().item()
-        scores_list.append(score)
-        mask_list.append(key)
-    # sort both lists by scores
-    scores_list, mask_list = zip(*sorted(zip(scores_list, mask_list)))
-    mask_list = list(mask_list)
-    mask_list.reverse()
-    scores_list = list(scores_list)
-    scores_list.reverse()
-    return mask_list, scores_list
+    for layer_idx in range(2):
+        for head_idx in range(8):
+            score = kl_dict[(layer_idx, head_idx)].sum() / mask_rep.sum().int().item()
+            scores_list[layer_idx, head_idx] = score
+    
+    # normalize by L2 of the layers
+    l2_norms = scores_list.norm(dim=0).unsqueeze(0)
+    scores_list = scores_list / l2_norms
 
+    all_heads = []
+    for layer_idx in range(2):
+        for head_idx in range(8):
+            all_heads.append((layer_idx, head_idx))
+
+    # sort both lists by scores
+    sorted_indices = sorted(all_heads, key=lambda x: scores_list[x])
+
+    # mask_list = list(mask_list)
+    # mask_list.reverse()
+    # scores_list = list(scores_list)
+    # scores_list.reverse()
+    # # return mask_list, scores_list
 
 # %%
-def mask_head(model, head_to_mask_tuple):
+
+def mask_head(model, head_to_mask_tuple, unmask=False):
     layer = head_to_mask_tuple[0]
     head = head_to_mask_tuple[1]
     for layer_index, layer_object in enumerate(model.blocks):
         for head_index in range(8):
             if layer_index == layer and head_index == head:
-                layer_object.attn.hook_result.xi.data[:, :, head, :] *= 0.0
-    return model
-
+                layer_object.attn.hook_result.xi.data[:, :, head, :] = (1.0 if unmask else 0.0)
 
 def remove_node(corr, node):
-    for child in node.children():
-        corr.remove_edge(child.name, child.index, node.name, node.index)
-    for parent in node.parents():
-        corr.remove_edge(node.name, node.index, parent.name, parent.index)
+    children = list(node.children)
+    print(children)
+    for child in children:
+        print("Removing edge", child.name, child.index, node.name, node.index)
+        try: corr.remove_edge(child.name, child.index, node.name, node.index)
+        except: pass
+    parents = list(node.parents)
+    print(parents)
+    for parent in parents:
+        print("Removing edge", node.name, node.index, parent.name, parent.index)
+        try: corr.remove_edge(node.name, node.index, parent.name, parent.index)
+        except: pass
     return corr
-
 
 def mask_head_in_correspondence(corr, head_to_mask_tuple):
     layer_to_mask = head_to_mask_tuple[0]
     head_to_mask = head_to_mask_tuple[1]
+
+    found = False
+
+    print("Masking...", head_to_mask_tuple)
     for node in corr.nodes():
-        head = node.index.hashable_tuple[-1]
-        layer = node.name.split(".")[1]
+        head = node.index.as_index[-1]
+        layer = int(node.name.split(".")[1])
+
         if layer == layer_to_mask and head == head_to_mask:
+            found=True
             corr = remove_node(corr, node)
+
+
+    assert found, head_to_mask_tuple
     return corr
 
+def count_no_edges(corr, verbose=False):
+    """TODO Arthur move this elsewhere"""
+
+    cnt = 0
+
+    for key, edge in corr.all_edges().items():
+        if edge.present and edge.edge_type != EdgeType.PLACEHOLDER:
+            if verbose:
+                print(key)
+            cnt += 1
+
+    if verbose:
+        print("No edge", cnt)
+
+    return cnt
 
 from acdc.TLACDCCorrespondence import TLACDCCorrespondence
-
 corr = TLACDCCorrespondence.setup_from_model(tl_model)
 
 # %%
-mask_list, scores_list = compute_scores(kls)
+
+sorted_indices = [ # precomputed
+    (1, 2),
+    (1, 4),
+    (1, 7),
+    (1, 1),
+    (0, 5),
+    (1, 3),
+    (1, 0),
+    (0, 6),
+    (1, 6),
+    (0, 0),
+    (0, 3),
+    (1, 5),
+    (0, 1),
+    (0, 7),
+    (0, 4),
+    (0, 2),
+]
+
+# mask_list, scores_list = compute_scores(kls)
 kl_div_list = []
 edge_list = []
-for index, score in enumerate(scores_list):
-    head_to_mask = mask_list[index]
-    model = mask_head(tl_model, head_to_mask)
-    corr = mask_head_in_correspondence(corr, head_to_mask)
-    # edges = corr.count_edges() # how to count edges?
-    kl_div = metric(model(toks_int_values))
-    kl_div_list.append(torch.mean(kl_div))
+
+print(metric(tl_model(toks_int_values)), "should be rough 0")
+
+for index in sorted_indices:
+    mask_head(tl_model, index)
+    # mask_head(corr, index)
+    corr = mask_head_in_correspondence(corr, index)
+    edges = count_no_edges(corr)
+    kl_div = metric(tl_model(toks_int_values))
+    kl_div_list.append(kl_div.sum().item() / mask_rep.sum().int().item())
     edge_list.append(edges)
+
+
 
 # %%
