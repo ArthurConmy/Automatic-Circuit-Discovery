c7dc7114a04d435838aef7953f1093b711716889
Arthur Conmy <arthurconmy@gmail.com>
2023-05-10 00:33:24 +0100

    Commit the update

--- acdc/main.py
+++ acdc/main.py
@@ -120,7 +120,7 @@ parser.add_argument('--single-step', action='store_true', help='Use single step,
 if IPython.get_ipython() is not None: # heheh get around this failing in notebooks
     # args = parser.parse_args("--threshold 1.733333 --zero-ablation".split())
     # args = parser.parse_args("--threshold 0.001 --using-wandb".split())
-    args = parser.parse_args("--task induction --using-wandb --threshold 0.04 --zero-ablation --wandb-project-name acdc --indices-mode reverse --first-cache-cpu False --second-cache-cpu False".split()) # TODO figure out why this is such high edge count...
+    args = parser.parse_args("--task docstring --using-wandb --threshold 0.04 --zero-ablation --wandb-project-name acdc --indices-mode reverse --first-cache-cpu False --second-cache-cpu False".split()) # TODO figure out why this is such high edge count...
 else:
     args = parser.parse_args()
 
