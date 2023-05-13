f6dde2ea4cc3207d8dce39d294f0601620aadd0c
Arthur Conmy <arthurconmy@gmail.com>
2023-05-13 11:50:31 +0100

    Add some script update

--- acdc/script.py
+++ acdc/script.py
@@ -1,37 +1,45 @@
+import os
 import subprocess
 import numpy as np 
+import multiprocessing
 from math import gcd
 
-START = -3
-STOP = 1
-div = 2
+used = set()
 
-thresholds = 10 ** np.linspace(-2, 0.5, 21)
+def run_script(threshold, gpu_id):
+    env = os.environ.copy()
+    env["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
+    subprocess.run(["python", "main.py", "--task", "ioi", "--wandb-run-name", str(threshold), "--wandb-project-name", "arthur_greaterthan_sweep", "--using-wandb", "--threshold", str(threshold), "--indices-mode", "reverse", "--first-cache-cpu", "False", "--second-cache-cpu", "False"], env=env)
 
-for it in range(3, int(1e6)):
-    # curspace = np.linspace(0.05, 0.1, it)
-    # curspace = np.logspace(-2, -1, it)
-    # curspace = [
-    #     # 0.075,
-    #     # 0.4,
-    #     # 0.5,
-    #     # 0.3,
-    #     # 0.25,
-    #     # 0.2,
-    #     0.067,
-    # ]
-    curspace = [t for t in thresholds]
+if __name__ == '__main__':
 
-    if not isinstance(curspace, list):
-        curspace = curspace[1:-1]
+    num_gpus = 8 # specify the number of GPUs available
+    num_jobs_per_gpu = 2 # specify the number of jobs per GPU
 
-    print(curspace)
-    for threshold_idx, threshold in list(enumerate(curspace)):
+    pool = multiprocessing.Pool(num_gpus * num_jobs_per_gpu)
+    jobs = []
+
+    for it in range(3, int(1e6)):
+        curspace = np.linspace(0.02, 0.07, it) # for IOI
+
         if not isinstance(curspace, list):
-            if gcd(threshold_idx, it) != 1:
-                continue
-        
-        subprocess.run(["python", "main.py", "--task", "induction", "--wandb-run-name", str(threshold), "--wandb-project-name", "arthur_zeros", "--zero-ablation", "--using-wandb", "--threshold", str(threshold), "--indices-mode", "reverse"])
+            curspace = curspace[1:-1]
 
-    if isinstance(curspace, list):
-        break
+        for threshold_idx, threshold in list(enumerate(curspace)):
+            if threshold in used:
+                continue
+            used.add(threshold)
+
+            gpu_id = (threshold_idx // num_jobs_per_gpu) % num_gpus
+            jobs.append(pool.apply_async(run_script, (threshold, gpu_id)))
+
+        if isinstance(curspace, list):
+            break
+
+    # wait for all jobs to finish
+    for job in jobs:
+        job.get()
+
+    # clean up
+    pool.close()
+    pool.join()
