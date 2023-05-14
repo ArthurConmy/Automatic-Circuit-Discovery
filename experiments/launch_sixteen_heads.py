206d40fa73d2b47e7b7dfb2ec718bd8de61f4f1e
Arthur Conmy <arthurconmy@gmail.com>
2023-05-14 15:02:05 +0100

    Last p

--- experiments/launch_sixteen_heads.py
+++ experiments/launch_sixteen_heads.py
@@ -51,7 +51,6 @@ import plotly.express as px
 import plotly.io as pio
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-pio.renderers.default = "colab"
 from acdc.hook_points import HookedRootModule, HookPoint
 from acdc.graphics import show
 from acdc.HookedTransformer import (
