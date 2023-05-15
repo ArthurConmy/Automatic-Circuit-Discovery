dfc528c5d2dc04788dd5ce0e08b20fcd11840c6a
Arthur Conmy <arthurconmy@gmail.com>
2023-05-15 21:39:58 +0100

    Zero and position make it good

--- acdc/main.py
+++ acdc/main.py
@@ -129,7 +129,7 @@ parser.add_argument('--single-step', action='store_true', help='Use single step,
 if IPython.get_ipython() is not None: # heheh get around this failing in notebooks
     # args = parser.parse_args("--threshold 1.733333 --zero-ablation".split())
     # args = parser.parse_args("--threshold 0.001 --using-wandb".split())
-    args = parser.parse_args("--task tracr-reverse --using-wandb --threshold 0.005 --wandb-project-name acdc --indices-mode reverse --first-cache-cpu False --second-cache-cpu False".split())
+    args = parser.parse_args("--task tracr-reverse --using-wandb --threshold 0.005 --zero-ablation --wandb-project-name acdc --indices-mode reverse --first-cache-cpu False --second-cache-cpu False".split())
 else:
     args = parser.parse_args()
 
