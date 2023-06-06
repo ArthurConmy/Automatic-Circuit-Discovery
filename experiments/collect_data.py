b577ee9a2559f1960bf304e10eeb5401b2f73a89
Arthur Conmy <conmy.a@northeastern.edu>
2023-06-06 16:18:41 +0000

    Scratchpad

--- experiments/collect_data.py
+++ experiments/collect_data.py
@@ -1,3 +1,23 @@
+"""
+Rip, I deleted the good edits to 
+
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+        modified:   experiments/all_roc_plots.py
+        modified:   experiments/collect_data.py
+        modified:   experiments/induction_results.py
+        modified:   experiments/launch_abstract.py
+        modified:   experiments/launch_sixteen_heads.py
+        modified:   experiments/launch_spreadsheet.py
+        modified:   notebooks/auc_tables.py
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        acdc/media/
+        ob-jupyter/
+"""
+
 import subprocess
 import argparse
 import os
