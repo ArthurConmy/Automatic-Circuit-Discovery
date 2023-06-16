a9b53ac7a4b6b8e3af96caaf2c827a50c664c488
Arthur Conmy <conmy.a@northeastern.edu>
2023-06-16 20:48:29 +0000

    Update to newer ACDC version

--- acdc/main.py
+++ acdc/main.py
@@ -27,7 +27,7 @@ try:
 
     ipython.run_line_magic( # install ACDC
         "pip",
-        "install git+https://github.com/ArthurConmy/Automatic-Circuit-Discovery.git@541ea29",
+        "install git+https://github.com/ArthurConmy/Automatic-Circuit-Discovery.git@9d5844a",
     )
 
 except Exception as e:
