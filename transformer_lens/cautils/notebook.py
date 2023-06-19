d36948c12d22fa8484169c0860257868f2ea8452
Arthur Conmy <conmy.a@northeastern.edu>
2023-06-19 13:54:55 +0000

    Update to notebook import different

---
+++ transformer_lens/cautils/notebook.py
@@ -0,0 +1,10 @@
+import warnings
+from IPython import get_ipython
+ipython = get_ipython()
+
+if ipython is not None: # so this works as a script and in a notebook
+    warnings.warn("Running load_ext autoreload...")
+    ipython.run_line_magic("load_ext", "autoreload")
+    ipython.run_line_magic("autoreload", "2")
+
+from transformer_lens.cautils.script import *
--- transformer_lens/cautils/utils.py
+++ transformer_lens/cautils/utils.py
@@ -1,12 +1,4 @@
 import warnings
-from IPython import get_ipython
-ipython = get_ipython()
-
-
-if ipython is not None: # so this works as a script and in a notebook
-    warnings.warn("Running load_ext autoreload...")
-    ipython.run_line_magic("load_ext", "autoreload")
-    ipython.run_line_magic("autoreload", "2")
 
 import torch as t
 import torch
