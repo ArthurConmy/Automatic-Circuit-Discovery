c8d09785b21f2733f2af1d947496a9946b79eb39
Arthur Conmy <conmy.a@northeastern.edu>
2023-06-28 16:28:27 +0000

    Check the top activating IOI dataset examples

--- transformer_lens/rs/arthurs_notebooks/arthur_signal_owt.py
+++ transformer_lens/rs/arthurs_notebooks/arthur_signal_owt.py
@@ -43,31 +43,71 @@ clear_output()
 #%%
 
 update_word_lists = {
-    " John": "Today John was reading a book when suddenly John heard a strange noise",
-    " Maria": "Today Maria loves playing the piano and, moreover Maria also enjoys painting",
-    " city": " The city was full of lights, making the city look like a sparkling diamond",
-    " ball": " The ball rolled away, so the dog chased the ball all the way to the park",
-    " Python": "Nowadays Python is a popular language for programming. In fact, Python is known for its simplicity",
-    " President": " The President announced new policies today. Many are waiting to see how the President's decisions will affect the economy",
-    " Bitcoin": "Recently, Bitcoin's value has been increasing rapidly. Investors are closely watching Bitcoin's performance",
-    " dog": " The dog wagged its tail happily. Seeing the dog so excited, the children started laughing",
-    " cake": " The cake looked delicious. Everyone at the party was eager to taste the cake today",
-    " book": " The book was so captivating, I couldn't put the book down",
-    " house": " The house was quiet. Suddenly, a noise from the upstairs of the house startled everyone",
-    " car": " The car pulled into the driveway. Everyone rushed out to see the new car today",
-    " computer": " The computer screen flickered. She rebooted the computer hoping to resolve the issue",
-    " key": " She lost the key to her apartment. She panicked when she realized she had misplaced the key today",
-    " apple": " He took a bite of the apple. The apple was crisp and delicious",
-    " phone": " The phone rang in the middle of the night. She picked up the phone with a groggy hello",
-    " train": " The train was late. The passengers were annoyed because the train was delayed by an hour",
+    " station": """" including the Kibo and Columbus science modules, even seem to reflect the Earth's 
+lovely bluish colors. The image also shows off large power generating solar arrays on the station's 90 meter long 
+integrated truss structure Just put your cursor over the picture to identify some of the major parts of the""",
+    " Roger": """Senators send letter to Roger GoodellSteve Delsohn reports the details surrounding the domestic 
+violence charge against Panthers""",
+    " overhead": """In addition, I attempted to lower the 
+resolution of the shot to fit the other two better. Again, the first image in this series is the overhead,
+the second two are from the Kim""",
+    " exploitation": """Thus a husband
+has a clear incentive to appropriate the wife��s future quasi-rents, by divorcing her unilaterally after having 
+extracted most of his quasi-rent from the marriage. This is called quasi-rent destruction.While the example is 
+provably the exception, it still is helpful in illustrating the concept. Clearly if a man was able to get away with
+this he would be rewarded materially for betraying his wife. But divorce theft isn��t the only option available to 
+the spouse which has the other one over a barrel. They could also use this change in fortunes to renegotiate the 
+terms of the marriage in their favor under threat of divorce, which economists call exploitation:Brinig and Allen 
+(2000) argue that there are two different types of quasi""",
+    " site": """Fuqua Development has unveiled tentative plans for a major mixed-use development wedged against 
+Memorial Drive and the Beltline corridor.A new marketing flyer released by the controversial developer elaborates 
+on his planned project in Reynoldstown, which includes more than 100,000 square feet of retail and restaurant space
+— including (potentially) a CineBistro and Sprouts — plus 120,000 square feet of offices, 600 apartments, and 100 
+condos.Fuqua purchased the site at 905 Memorial Drive last year, which didn't thrill a lot of folks, given the 
+developer's penchant for big-box projects. Fuqua's planned overhaul will replace a warehouse facility butted up 
+against Interstate 20.According to this rendering, which might not represent the latest version of Fuqua's plans, 
+retail spaces would front the Belt""",
+    " use": """Synopsis EditProduction EditMusic EditMeaning EditReggio stated that the Qatsi films are intended to 
+simply create an experience and that "it is up  the viewer to take for himself/herself what it is that  means." He 
+also said that "these films have never been about the effect of technology, of industry on people. It's been that 
+everyone: politics, education, things of the financial structure, the nation state structure, language, the 
+culture, religion, all of that exists within the host of technology. So it's not the effect of, it's that 
+everything exists within . It's not that we use technology, we live""",
+    " oil": """ROME (Thomson Reuters Foundation) - Italian police said they have busted a crime ring exporting fake 
+extra virgin olive oil to the United States, highlighting the mafia��s infiltration of Italy��s famed agriculture 
+and food business.Twelve people with links to the ��Ndrangheta, the organized crime group based in the southern 
+Calabria region, were arrested on Tuesday on a series of charges including mafia association and fraud, police said
+in a statement.The gang shipped cheap olive p""",
+#     " with": """ Park next month.��I am really looking forward to the inaugural SprintX race at Canadian Tire Motorsport Park.�� 
+# Wittmer said.��Racing in my home country is always as nice thing, especially in front of family, friends, and 
+# representing the BMW brand.��Wittmer, a BMW of North America factory driver, is a former IMSA GT Le Mans class 
+# champion, and will bring a wealth of experience to the upstart team, which made its debut at Circuit of The 
+# Americas last month.The Texas-based team, with the support BMW of North America, has shifted its focus entirely to 
+# SprintX for the remainder of the year.��I��m honored to have the opportunity to participate with Kuno for this new 
+# series,�� Mills said.��It��s a dream come true to be included in the BMW family, and we are excited to pursue a 
+# championship with Kun""",
+    " resolve": """ Each time TTC staff were forced to respond it took an average of 4 minutes 51 seconds to get things moving 
+again.The TTC counts incidents that involve the police separately, labeling them "security incidents."Just behind 
+passenger alerts was the TTC's own train problems. The rolling stock - a technical name for the trains - needed 
+emergency repairs 1,323 times in 2012, causing 109 hours and 49 minutes of delays.Frustratingly, the problem that 
+created the worst wait times is almost entirely preventable. 330 small fires, litter problems, and unauthorized 
+people at track level stopped the subway for a total of 106 hours and 37 minutes in 2012. Each incident took an 
+average of 19 minutes to resolve - longer than breakdowns and assistance alarms.In total there were 4,842 outages 
+on the subway in 2012 that caused more than three weeks of delays (509 hours and 35 minutes to be precise.) The 
+average delay across all types took 6 minutes and 31 seconds to clear""",
 }
 
 ks = list(update_word_lists.keys())
 for i, k in enumerate(ks):
-    while not update_word_lists[k].endswith(k):
-        update_word_lists[k] = update_word_lists[k][:-1]
-    if i not in [3, 4, 6, 7, 8, 13, 15, 16]:
-        update_word_lists.pop(k)
+
+    # if i not in [3,5,7]: 
+    #     update_word_lists.pop(k)
+    #     continue
+
+    # cur_sent = model.to_tokens([update_word_lists[k]])[0][-1].item()
+    update_word_lists[k] = model.tokenizer.decode(model.to_tokens([update_word_lists[k]])[0][:-1])+k  # cut off last thing
+    # while not update_word_lists[k].endswith(k):
+    #     update_word_lists[k] = update_word_lists[k][:-1]
 
 if USE_IOI:
     N = 60
@@ -92,60 +132,9 @@ W_E = model.W_E
 # Define an easier-to-use dict!
 effective_embeddings = {"W_EE": W_EE, "W_EE0": W_EE0, "W_E": W_E}
 
-#%%
-
-res = []
-
-#%%
-
-res.append(decompose_attn_scores_full(
-    ioi_dataset=ioi_dataset,
-    batch_size = N,
-    seed = 0,
-    nnmh = (10, 7),
-    model = model,
-    use_effective_embedding = False,
-    use_layer0_heads = False,
-    subtract_S1_attn_scores = True,
-    include_S1_in_unembed_projection = False,
-    project_onto_comms_space = "W_EE0A",
-))
-
-create_fucking_massive_plot_1(res[0])
-
-# %%
-
-create_fucking_massive_plot_2(res[0])
-
-# %%
-
-ioi_fake = FakeIOIDataset(
-    sentences = ioi_dataset.sentences,
-    io_tokens = [" " + sent.split(" ")[-1] for sent in ioi_dataset.sentences],
-    key_increment=0,
-    model=model,
-)
-ioi_fake.s_tokenIDs = ioi_dataset.s_tokenIDs
-ioi_fake.word_idx["S1"] = ioi_dataset.word_idx["S1"]
-
-# %%
-
-res2 = decompose_attn_scores_full(
-    ioi_dataset=ioi_fake,
-    batch_size = N,
-    seed = 0,
-    nnmh = (10, 7),
-    model = model,
-    use_effective_embedding = False,
-    use_layer0_heads = False,
-    subtract_S1_attn_scores = True,
-    include_S1_in_unembed_projection = False,
-    project_onto_comms_space = "W_EE0A",
-)
 # %%
 
 # yey fake thing works
-
 data1 = FakeIOIDataset(
     sentences = list(update_word_lists.values()),
     io_tokens=list(update_word_lists.keys()),
@@ -156,9 +145,7 @@ data1 = FakeIOIDataset(
 # %%
 
 res = {}
-
-for inc in tqdm([1, 2]):
-
+for inc in tqdm([-2, -1, 1, 2]):
     data1 = FakeIOIDataset(
         sentences = list(update_word_lists.values()),
         io_tokens=list(update_word_lists.keys()),
@@ -183,24 +170,19 @@ create_fucking_massive_plot_1(
     sum(list(res.values()))/(len(res)),
 )
 
-# %%
-
-create_fucking_massive_plot_2(
-    sum(list(res.values()))/(len(res)),
-)
-
 #%%
 
-logits, cache = model.run_with_cache(
-    data1.toks,
-    names_filter=lambda name: name.endswith("pattern"),
-)
+px.bar( 
+    x=[
+        "q ∥ W<sub>U</sub>[IO], k ∥ MLP<sub>0</sub>",
+        "q ∥ W<sub>U</sub>[IO], k ⊥ MLP<sub>0</sub>", 
+        "q ⊥ W<sub>U</sub>[IO] & ∥ comms, k ∥ MLP<sub>0</sub>",
+        "q ⊥ W<sub>U</sub>[IO] & ∥ comms, k ⊥ MLP<sub>0</sub>", 
+        "q ⊥ W<sub>U</sub>[IO] & ⟂ comms, k ∥ MLP<sub>0</sub>", 
+        "q ⊥ W<sub>U</sub>[IO] & ⟂ comms, k ⊥ MLP<sub>0</sub>"
+    ],
+    y=100*(sum(res.values()).sum(dim=(1, 2)) / sum(res.values()).sum()),
+    title="Percentage contribution to attention scores, averaged over 'S1' positions which we take the mean over",
+).show()
 
-#%%
-
-imshow(
-    cache["blocks.10.attn.hook_pattern"][torch.arange(data1.N), 7, data1.word_idx["end"], data1.word_idx["IO"]].unsqueeze(0)
-)
 # %%
-
-# 3, 4, 6, 7, 8, 13, 15, 16
