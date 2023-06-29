d3426a731b32a16546718d61c82bf0737ecdc199
Arthur Conmy <conmy.a@northeastern.edu>
2023-06-29 01:05:13 +0000

    Add a measure that does work, but it is much less ambitious

--- transformer_lens/rs/arthurs_notebooks/key_and_query_projection.py
+++ transformer_lens/rs/arthurs_notebooks/key_and_query_projection.py
@@ -10,6 +10,7 @@ from transformer_lens.rs.callum.keys_fixed import project, get_effective_embeddi
 from transformer_lens.rs.arthurs_notebooks.arthur_utils import *
 import argparse
 
+
 #%%
 
 model = HookedTransformer.from_pretrained(
@@ -33,290 +34,286 @@ effective_embeddings = get_effective_embedding_2(model)
 # Do this crap
 # See change in loss
 
-NEGATIVE_LAYER_IDX, NEGATIVE_HEAD_IDX = NEG_HEADS[model.cfg.model_name]
+# NEGATIVE_LAYER_IDX, NEGATIVE_HEAD_IDX = NEG_HEADS[model.cfg.model_name]
 # NEGATIVE_HEAD_IDX, NEGATIVE_LAYER_IDX = 9, 9
 
-# for NEGATIVE_LAYER_IDX, NEGATIVE_HEAD_IDX in [(10, 7)] + list(itertools.product(range(11, -1, -1), range(12))):
+for NEGATIVE_LAYER_IDX, NEGATIVE_HEAD_IDX in [(10, 7), (9,9)] + list(itertools.product(range(11, -1, -1), range(12))):
     # NEG_HEADS[model.cfg.model_name]:
 
-END_STATE_HOOK = f"blocks.{model.cfg.n_layers-1}.hook_resid_post"
-names_filter1 = (
-    lambda name: name == END_STATE_HOOK
-    or name==f"blocks.{NEGATIVE_LAYER_IDX}.hook_resid_pre"
-    or name==f"blocks.{NEGATIVE_LAYER_IDX}.attn.hook_result"
-)
-model = model.to("cuda:1")
-logits, cache = model.run_with_cache(
-    batched_tokens.to("cuda:1"),
-    names_filter=names_filter1,
-    device="cpu",
-)
-model = model.to("cuda:0")
-cache.to("cuda:0")
-print("Done")
-cpu_logits = logits.cpu()
-del logits
-gc.collect()
-torch.cuda.empty_cache()
+    END_STATE_HOOK = f"blocks.{model.cfg.n_layers-1}.hook_resid_post"
+    names_filter1 = (
+        lambda name: name == END_STATE_HOOK
+        or name==f"blocks.{NEGATIVE_LAYER_IDX}.hook_resid_pre"
+        or name==f"blocks.{NEGATIVE_LAYER_IDX}.attn.hook_result"
+    )
+    model = model.to("cuda:1")
+    logits, cache = model.run_with_cache(
+        batched_tokens.to("cuda:1"),
+        names_filter=names_filter1,
+        device="cpu",
+    )
+    model = model.to("cuda:0")
+    cache.to("cuda:0")
+    print("Done")
+    cpu_logits = logits.cpu()
+    del logits
+    gc.collect()
+    torch.cuda.empty_cache()
 
-# %%
+    # %%
 
-original_end_state = cache[get_act_name("resid_post", model.cfg.n_layers-1)]
+    original_end_state = cache[get_act_name("resid_post", model.cfg.n_layers-1)]
 
-batched_tokens_loss = get_loss_from_end_state(
-    model=model,
-    end_state=original_end_state,
-    targets=targets,
-)
+    batched_tokens_loss = get_loss_from_end_state(
+        model=model,
+        end_state=original_end_state,
+        targets=targets,
+    )
 
-#%%
+    #%%
 
-head_output = cache[get_act_name("result", NEGATIVE_LAYER_IDX)][:, :, NEGATIVE_HEAD_IDX]
-assert head_output.shape == (BATCH_SIZE, model.cfg.n_ctx, model.cfg.d_model)
+    head_output = cache[get_act_name("result", NEGATIVE_LAYER_IDX)][:, :, NEGATIVE_HEAD_IDX]
+    assert head_output.shape == (BATCH_SIZE, model.cfg.n_ctx, model.cfg.d_model)
 
-#%%
+    #%%
 
-unembed = einops.einsum(
-    head_output, 
-    model.W_U,
-    "b s d_model, d_model d_vocab -> b s d_vocab",
-)
-
-#%% 
-
-the_topk = torch.topk(
-    -unembed,
-    k=10,
-    dim=-1,
-).indices
-
-
-#%%
-
-mean_head_output = einops.reduce(head_output, "b s d -> d", reduction="mean")
-
-#%%
-
-mean_ablated_end_states = cache[get_act_name("resid_post", model.cfg.n_layers-1)] - head_output + einops.repeat(mean_head_output, "d -> b s d", b=BATCH_SIZE, s=model.cfg.n_ctx)
-mean_ablated_loss = get_loss_from_end_state(
-    model=model,
-    end_state=mean_ablated_end_states,
-    targets=targets,
-)
-
-# %%
-
-max_importance_examples = sorted(
-    [
-        (
-            batch_idx,
-            seq_idx,
-            (mean_ablated_loss-batched_tokens_loss)[batch_idx, seq_idx].item(),
+    if ipython is not None:
+        unembed = einops.einsum(
+            head_output, 
+            model.W_U,
+            "b s d_model, d_model d_vocab -> b s d_vocab",
         )
-        for batch_idx, seq_idx in itertools.product(
-            range(BATCH_SIZE), range(model.cfg.n_ctx)
+
+    #%% 
+
+    if ipython is not None:
+        the_topk = torch.topk(
+            -unembed,
+            k=10,
+            dim=-1,
+        ).indices
+
+
+    #%%
+
+    mean_head_output = einops.reduce(head_output, "b s d -> d", reduction="mean")
+
+    #%%
+
+    mean_ablated_end_states = cache[get_act_name("resid_post", model.cfg.n_layers-1)] - head_output + einops.repeat(mean_head_output, "d -> b s d", b=BATCH_SIZE, s=model.cfg.n_ctx)
+    mean_ablated_loss = get_loss_from_end_state(
+        model=model,
+        end_state=mean_ablated_end_states,
+        targets=targets,
+    )
+
+    # %%
+
+    max_importance_examples = sorted(
+        [
+            (
+                batch_idx,
+                seq_idx,
+                (mean_ablated_loss-batched_tokens_loss)[batch_idx, seq_idx].item(),
+            )
+            for batch_idx, seq_idx in itertools.product(
+                range(BATCH_SIZE), range(model.cfg.n_ctx)
+            )
+        ],
+        key=lambda x: x[2],
+        reverse=True,
+    )
+
+    # %%
+
+    # Get the top 5% of things by importance
+    all_top_5_percent = max_importance_examples[: BATCH_SIZE]
+
+    warnings.warn("No shuffling atm")
+    # DONT shuffle them
+    # np.random.seed(799)
+    # np.random.shuffle(all_top_5_percent)
+    top_5_percent = all_top_5_percent[: BATCH_SIZE]
+
+    top5p_batch_indices = [x[0] for x in top_5_percent]
+    top5p_seq_indices = [x[1] for x in top_5_percent]
+
+    #%%
+
+    top5p_tokens = batched_tokens[top5p_batch_indices]
+    top5p_targets = torch.LongTensor([targets[top5p_batch_idx, top5p_seq_idx] for top5p_batch_idx, top5p_seq_idx in zip(top5p_batch_indices, top5p_seq_indices)])
+
+    #%%
+
+    top5p_losses = batched_tokens_loss[top5p_batch_indices, top5p_seq_indices]
+
+    # %%
+
+    # Do the key-side thing where we project onto W_U
+    # selected_unembeddings = cache[get_act_name("resid_pre", NEGATIVE_LAYER_IDX)][torch.tensor(top5p_batch_indices), torch.tensor(top5p_seq_indices)]
+
+    keyside_projections = t.zeros((BATCH_SIZE, model.cfg.n_ctx, model.cfg.d_model))
+    keyside_orthogonals = t.zeros((BATCH_SIZE, model.cfg.n_ctx, model.cfg.d_model))
+
+    for batch_idx, seq_idx in tqdm(list(itertools.product(range(BATCH_SIZE), range(model.cfg.n_ctx)))):
+        keyside_vector, keyside_orthogonal = project(
+            cache[get_act_name("resid_pre", NEGATIVE_LAYER_IDX)][batch_idx, seq_idx],
+            effective_embeddings["W_E (including MLPs)"][batched_tokens[batch_idx, seq_idx]],
         )
-    ],
-    key=lambda x: x[2],
-    reverse=True,
-)
+        keyside_projections[batch_idx, seq_idx] = keyside_vector
+        keyside_orthogonals[batch_idx, seq_idx] = keyside_orthogonal
 
-# %%
+    queryside_vectors = t.zeros((BATCH_SIZE, model.cfg.d_model)).cuda()
 
-# Get the top 5% of things by importance
-all_top_5_percent = max_importance_examples[: BATCH_SIZE]
+    # just do this part for the individual queries that we need
+    for batch_batch_idx, (batch_idx, seq_idx) in enumerate(list(zip(top5p_batch_indices, 
+    top5p_seq_indices))):
 
-warnings.warn("No shuffling atm")
-# DONT shuffle them
-# np.random.seed(799)
-# np.random.shuffle(all_top_5_percent)
-top_5_percent = all_top_5_percent[: BATCH_SIZE]
+        queryside_vector, queryside_orthogonal = project(
+            cache[get_act_name("resid_pre", NEGATIVE_LAYER_IDX)][batch_idx, seq_idx],
+            dir=[model.W_U.T[top5p_tokens[batch_idx, earlier_seq_idx]] for earlier_seq_idx in range(seq_idx+1)],
+        )
+        queryside_vectors[batch_batch_idx] = queryside_vector
+        warnings.warn("ANtoerh")
+        queryside_vectors[batch_batch_idx] = model.W_U.T[top5p_tokens[batch_idx, seq_idx]]
 
-top5p_batch_indices = [x[0] for x in top_5_percent]
-top5p_seq_indices = [x[1] for x in top_5_percent]
+    #%%
 
-#%%
+    new_k_input = t.zeros((BATCH_SIZE, model.cfg.n_ctx, model.cfg.d_model))
 
-top5p_tokens = batched_tokens[top5p_batch_indices]
-top5p_targets = torch.LongTensor([targets[top5p_batch_idx, top5p_seq_idx] for top5p_batch_idx, top5p_seq_idx in zip(top5p_batch_indices, top5p_seq_indices)])
+    for batch_batch_idx, batch_idx in enumerate(top5p_batch_indices):
+    
+        warnings.warn("Writing as literally the unembed...")
+    
+        new_k_input[batch_batch_idx] = torch.stack([
+            effective_embeddings["W_E (including MLPs)"][batched_tokens[batch_idx, seq_idx]] for seq_idx in range(model.cfg.n_ctx)
+        ])
 
-#%%
+        # new_k_input[batch_batch_idx] = torch.stack([
+        #     keyside_projections[batch_idx, seq_idx] for seq_idx in range(model.cfg.n_ctx)
+        # ])
 
-top5p_losses = batched_tokens_loss[top5p_batch_indices, top5p_seq_indices]
+    #%%
 
-# %%
+    model.set_use_split_qkv_input(True)
 
-# Do the key-side thing where we project onto W_U
-# selected_unembeddings = cache[get_act_name("resid_pre", NEGATIVE_LAYER_IDX)][torch.tensor(top5p_batch_indices), torch.tensor(top5p_seq_indices)]
-
-keyside_projections = t.zeros((BATCH_SIZE, model.cfg.n_ctx, model.cfg.d_model))
-keyside_orthogonals = t.zeros((BATCH_SIZE, model.cfg.n_ctx, model.cfg.d_model))
-
-for batch_idx, seq_idx in tqdm(list(itertools.product(range(BATCH_SIZE), range(model.cfg.n_ctx)))):
-    keyside_vector, keyside_orthogonal = project(
-        cache[get_act_name("resid_pre", NEGATIVE_LAYER_IDX)][batch_idx, seq_idx],
-        effective_embeddings["W_E (only MLPs)"][batched_tokens[batch_idx, seq_idx]],
+    model.reset_hooks()
+    model.add_hook(
+        get_act_name("k_input", NEGATIVE_LAYER_IDX),
+        partial(set_to_value, head_idx=NEGATIVE_HEAD_IDX, new_value=new_k_input.to("cuda:1")),
+        level=1,
     )
-    keyside_projections[batch_idx, seq_idx] = keyside_vector
-    keyside_orthogonals[batch_idx, seq_idx] = keyside_orthogonal
-
-queryside_vectors = t.zeros((BATCH_SIZE, model.cfg.d_model)).cuda()
-
-# just do this part for the individual queries that we need
-for batch_batch_idx, (batch_idx, seq_idx) in enumerate(list(zip(top5p_batch_indices, 
-top5p_seq_indices))):
-
-    queryside_vector, queryside_orthogonal = project(
-        cache[get_act_name("resid_pre", NEGATIVE_LAYER_IDX)][batch_idx, seq_idx],
-        dir=[model.W_U.T[top5p_tokens[batch_idx, earlier_seq_idx]] for earlier_seq_idx in range(seq_idx+1)],
+    # model.add_hook(
+    #     get_act_name("q_input", NEGATIVE_LAYER_IDX),
+    #     partial(set_to_value, head_idx=NEGATIVE_HEAD_IDX, seq_indices = top5p_seq_indices, new_value=queryside_vectors.to("cuda:1")),
+    #     level=1,
+    # )
+    model.to("cuda:1")
+    logits, top_5p_cache = model.run_with_cache(
+        top5p_tokens.to("cuda:1"),
+        names_filter = lambda name: name in [get_act_name("result", NEGATIVE_LAYER_IDX), get_act_name("resid_post", model.cfg.n_layers-1), get_act_name("pattern", NEGATIVE_LAYER_IDX)],
+        device="cuda:0"
     )
-    queryside_vectors[batch_batch_idx] = queryside_vector
-    warnings.warn("ANtoerh")
-    queryside_vectors[batch_batch_idx] = model.W_U.T[top5p_tokens[batch_idx, seq_idx]]
+    model.reset_hooks()
+    model.to("cuda:0")
+    new_head_out = top_5p_cache[get_act_name("result", NEGATIVE_LAYER_IDX)][torch.arange(len(top5p_tokens)), top5p_seq_indices, NEGATIVE_HEAD_IDX].unsqueeze(0)
 
-#%%
+    relevant_head_outs = []
+    for batch_idx, seq_idx in zip(top5p_batch_indices, top5p_seq_indices):
+        relevant_head_outs.append(
+            head_output[batch_idx, seq_idx]
+        )
+    relevant_head_outs = t.stack(relevant_head_outs).unsqueeze(0)
 
-new_k_input = t.zeros((BATCH_SIZE, model.cfg.n_ctx, model.cfg.d_model))
+    #%%
 
-for batch_batch_idx, batch_idx in enumerate(top5p_batch_indices):
- 
-    warnings.warn("Writing as literally the unembed...")
- 
-    new_k_input[batch_batch_idx] = torch.stack([
-        effective_embeddings["W_E (including MLPs)"][batched_tokens[batch_idx, seq_idx]] for seq_idx in range(model.cfg.n_ctx)
-    ])
+    if ipython is not None:
+        new_head_unembed = einops.einsum(
+            new_head_out[0],
+            model.W_U,
+            "seq_len dim, dim vocab -> seq_len vocab",
+        )
+        new_head_topk = t.topk(-new_head_unembed, k=10, dim=-1).indices
 
-    # new_k_input[batch_batch_idx] = torch.stack([
-    #     keyside_projections[batch_idx, seq_idx] for seq_idx in range(model.cfg.n_ctx)
-    # ])
+    #%%
 
-#%%
+    top_5p_end_state = original_end_state[top5p_batch_indices, top5p_seq_indices].unsqueeze(0)
+    assert top_5p_end_state.shape == relevant_head_outs.shape == new_head_out.shape, (top_5p_end_state.shape, relevant_head_outs.shape, new_head_out.shape)
 
-model.set_use_split_qkv_input(True)
+    top_5p_end_state -= relevant_head_outs
+    top_5p_end_state += new_head_out
 
-model.reset_hooks()
-model.add_hook(
-    get_act_name("k_input", NEGATIVE_LAYER_IDX),
-    partial(set_to_value, head_idx=NEGATIVE_HEAD_IDX, new_value=new_k_input.to("cuda:1")),
-    level=1,
-)
-# model.add_hook(
-#     get_act_name("q_input", NEGATIVE_LAYER_IDX),
-#     partial(set_to_value, head_idx=NEGATIVE_HEAD_IDX, seq_indices = top5p_seq_indices, new_value=queryside_vectors.to("cuda:1")),
-#     level=1,
-# )
-model.to("cuda:1")
-logits, top_5p_cache = model.run_with_cache(
-    top5p_tokens.to("cuda:1"),
-    names_filter = lambda name: name in [get_act_name("result", NEGATIVE_LAYER_IDX), get_act_name("resid_post", model.cfg.n_layers-1), get_act_name("pattern", NEGATIVE_LAYER_IDX)],
-    device="cuda:0"
-)
-model.reset_hooks()
-model.to("cuda:0")
-new_head_out = top_5p_cache[get_act_name("result", NEGATIVE_LAYER_IDX)][torch.arange(len(top5p_tokens)), top5p_seq_indices, NEGATIVE_HEAD_IDX].unsqueeze(0)
+    #%%
 
-relevant_head_outs = []
-for batch_idx, seq_idx in zip(top5p_batch_indices, top5p_seq_indices):
-    relevant_head_outs.append(
-        head_output[batch_idx, seq_idx]
+    loss = get_loss_from_end_state(
+        end_state=top_5p_end_state,
+        model=model,
+        logits=None,
+        targets=top5p_targets.unsqueeze(0),
     )
-relevant_head_outs = t.stack(relevant_head_outs).unsqueeze(0)
 
-#%%
+    #%%
 
-new_head_unembed = einops.einsum(
-    new_head_out[0],
-    model.W_U,
-    "seq_len dim, dim vocab -> seq_len vocab",
-)
-new_head_topk = t.topk(-new_head_unembed, k=10, dim=-1).indices
+    mean_ablation_loss = get_loss_from_end_state(
+        end_state=top_5p_end_state-new_head_out+mean_head_output,
+        logits=None,
+        targets=top5p_targets.unsqueeze(0),
+        model=model,
+    )
+    torch.testing.assert_close(
+        mean_ablation_loss[0],
+        mean_ablated_loss[top5p_batch_indices, top5p_seq_indices],
+        rtol=1e-3,
+        atol=1e-3,
+    )
 
+    #%%
 
-# for i in range(5):
-#     print()
-#     print(model.to_string(the_topk[top5p_batch_indices[i], top5p_seq_indices[i]]))
-#     print(model.to_string(new_head_topk[i]))
+    px.scatter(
+        x=top5p_losses.cpu().tolist(),
+        y=loss.cpu().tolist(),
+        labels={
+            "x": "Original Loss",
+            "y": "New Loss",
+        },
+        # title=f"{NEGATIVE_LAYER_IDX}.{NEGATIVE_HEAD_IDX} Losses for sample of Top 5% Direct Effect positions w/ both projections",
+        # text = [f"Batch {batch_idx}, Seq {seq_idx}" for batch_idx, seq_idx in zip(top5p_batch_indices, top5p_seq_indices)],
+    )
 
-#%%
+    # %%
 
-top_5p_end_state = original_end_state[top5p_batch_indices, top5p_seq_indices].unsqueeze(0)
-assert top_5p_end_state.shape == relevant_head_outs.shape == new_head_out.shape, (top_5p_end_state.shape, relevant_head_outs.shape, new_head_out.shape)
+    change_in_loss = (loss.cpu() - top5p_losses.cpu()).tolist()[0]
+    change_in_loss_mean = sum(change_in_loss)/len(change_in_loss)
+    top5pdata = top5p_losses.cpu().tolist()
+    lossdata = loss.cpu().tolist()
 
-top_5p_end_state -= relevant_head_outs
-top_5p_end_state += new_head_out
+    # # read the existing data
+    my_fname = "../arthur/json_data/approximations_with_key_lock_only.json"
+    with open(my_fname, "r") as f:
+        cur_json = json.load(f)
 
-#%%
+    mean_ablation_bad =  (mean_ablated_loss[top5p_batch_indices, top5p_seq_indices] - top5p_losses).tolist()
 
-loss = get_loss_from_end_state(
-    end_state=top_5p_end_state,
-    model=model,
-    logits=None,
-    targets=top5p_targets.unsqueeze(0),
-)
+    # update the data
+    cur_json[str((NEGATIVE_LAYER_IDX, NEGATIVE_HEAD_IDX))] = {
+        "layer_idx": NEGATIVE_LAYER_IDX,
+        "head_idx": NEGATIVE_HEAD_IDX,
+        "top5p_losses": top5pdata,
+        "losses": lossdata,
+        "change_in_loss": change_in_loss,
+        "change_in_loss_mean": change_in_loss_mean,
+        "mean_ablated_loss": [mean_ablated_loss[batch_idx, seq_idx].item() for batch_idx, seq_idx in zip(top5p_batch_indices, top5p_seq_indices)],
+        "time": ctime()+"_remember_sometimes_this_is_an_hour_too_early",
+        "how_bad_is_mean_ablation": mean_ablation_bad,
+        "how_bad_is_mean_ablation_mean": sum(mean_ablation_bad)/len(mean_ablation_bad),
+    }    
 
-#%%
+    # write the data
+    # write the updated data (don't overwrite!)
+    with open(my_fname, "w") as f:
+        f.write(json.dumps(cur_json, indent=4))
 
-mean_ablation_loss = get_loss_from_end_state(
-    end_state=top_5p_end_state-new_head_out+mean_head_output,
-    logits=None,
-    targets=top5p_targets.unsqueeze(0),
-    model=model,
-)
-torch.testing.assert_close(
-    mean_ablation_loss[0],
-    mean_ablated_loss[top5p_batch_indices, top5p_seq_indices],
-    rtol=1e-3,
-    atol=1e-3,
-)
+    # %%
 
-#%%
-
-px.scatter(
-    x=top5p_losses.cpu().tolist(),
-    y=loss.cpu().tolist(),
-    labels={
-        "x": "Original Loss",
-        "y": "New Loss",
-    },
-    # title=f"{NEGATIVE_LAYER_IDX}.{NEGATIVE_HEAD_IDX} Losses for sample of Top 5% Direct Effect positions w/ both projections",
-    # text = [f"Batch {batch_idx}, Seq {seq_idx}" for batch_idx, seq_idx in zip(top5p_batch_indices, top5p_seq_indices)],
-)
-
-# %%
-
-change_in_loss = (loss.cpu() - top5p_losses.cpu()).tolist()[0]
-change_in_loss_mean = sum(change_in_loss)/len(change_in_loss)
-top5pdata = top5p_losses.cpu().tolist()
-lossdata = loss.cpu().tolist()
-
-# # read the existing data
-my_fname = "../arthur/json_data/approximations_with_key_lock_only.json"
-with open(my_fname, "r") as f:
-    cur_json = json.load(f)
-cur_json = {}
-
-mean_ablation_bad =  (mean_ablated_loss[top5p_batch_indices, top5p_seq_indices] - top5p_losses).tolist()
-
-# update the data
-cur_json[str((NEGATIVE_LAYER_IDX, NEGATIVE_HEAD_IDX))] = {
-    "layer_idx": NEGATIVE_LAYER_IDX,
-    "head_idx": NEGATIVE_HEAD_IDX,
-    "top5p_losses": top5pdata,
-    "losses": lossdata,
-    "change_in_loss": change_in_loss,
-    "change_in_loss_mean": change_in_loss_mean,
-    "mean_ablated_loss": [mean_ablated_loss[batch_idx, seq_idx].item() for batch_idx, seq_idx in zip(top5p_batch_indices, top5p_seq_indices)],
-    "time": ctime()+"_remember_sometimes_this_is_an_hour_too_early",
-    "how_bad_is_mean_ablation": mean_ablation_bad,
-    "how_bad_is_mean_ablation_mean": sum(mean_ablation_bad)/len(mean_ablation_bad),
-}    
-
-# write the data
-# write the updated data (don't overwrite!)
-with open(my_fname, "w") as f:
-    f.write(json.dumps(cur_json, indent=4))
-
-# %%
-
-# now also try the q projection
-# %%
+    # now also try the q projection
+    # %%
