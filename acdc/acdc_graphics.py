56e0cb3d4a323fc724596b6e907871db815ab9e1
ArthurConmy <35957051+ArthurConmy@users.noreply.github.com>
2023-07-06 13:09:06 +0100

    Update acdc_graphics.py typing

--- acdc/acdc_graphics.py
+++ acdc/acdc_graphics.py
@@ -6,7 +6,7 @@ import torch
 import numpy as np
 import huggingface_hub
 import datetime
-from typing import Dict
+from typing import Dict, Union
 import wandb
 import plotly.graph_objects as go
 import torch
@@ -93,7 +93,7 @@ def build_colorscheme(correspondence, colorscheme: str = "Pastel2", show_full_in
 def show(
     correspondence: TLACDCCorrespondence,
     fname=None,
-    colorscheme: dict | str = "Pastel2",
+    colorscheme: Union[Dict, str] = "Pastel2",
     minimum_penwidth: float = 0.3,
     show_full_index: bool = True,
     remove_self_loops: bool = True,
