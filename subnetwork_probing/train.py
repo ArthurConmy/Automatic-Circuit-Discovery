45a201ca3b2e6b502ec9830db6980aaff78d3b03
Arthur Conmy <conmy.a@northeastern.edu>
2023-08-08 15:53:32 +0000

    to del fine

--- subnetwork_probing/train.py
+++ subnetwork_probing/train.py
@@ -36,7 +36,7 @@ import wandb
 
 
 def iterative_correspondence_from_mask(model: HookedTransformer, nodes_to_mask: list[TLACDCInterpNode],
-                                       use_pos_embed: bool = False,newv = False, corr: Optional[TLACDCCorrespondence] = None,
+                                       use_pos_embed: bool = False, corr: Optional[TLACDCCorrespondence] = None,
                                        head_parents: Optional[List] = None) -> TLACDCCorrespondence:
     if corr is None:
         corr = TLACDCCorrespondence.setup_from_model(model, use_pos_embed=use_pos_embed)
@@ -73,6 +73,50 @@ def iterative_correspondence_from_mask(model: HookedTransformer, nodes_to_mask: 
                 except KeyError:
                     pass
     return corr, head_parents
+
+def correspondence_from_mask(model: HookedTransformer, nodes_to_mask: list[TLACDCInterpNode], use_pos_embed: bool = False) -> TLACDCCorrespondence:
+    corr = TLACDCCorrespondence.setup_from_model(model, use_pos_embed=use_pos_embed)
+
+    additional_nodes_to_mask = []
+
+    # If all of {qkv} is masked, also add its head child
+    # to the list of nodes to mask
+    head_parents = collections.defaultdict(lambda: 0)
+    for node in nodes_to_mask:
+        additional_nodes_to_mask.append(TLACDCInterpNode(node.name.replace(".attn.", ".") + "_input", node.index, EdgeType.ADDITION))
+
+        if node.name.endswith("_q") or node.name.endswith("_k") or node.name.endswith("_v"):
+            child_name = node.name.replace("_q", "_result").replace("_k", "_result").replace("_v", "_result")
+            head_parents[(child_name, node.index)] += 1
+
+            # Forgot to add these in earlier versions of Subnetwork Probing, and so the edge counts were inflated
+            additional_nodes_to_mask.append(TLACDCInterpNode(child_name + "_input", node.index, EdgeType.ADDITION))
+        
+        if node.name.endswith(("mlp_in", "resid_mid")):
+            additional_nodes_to_mask.append(TLACDCInterpNode(node.name.replace("resid_mid", "mlp_out").replace("mlp_in", "mlp_out"), node.index, EdgeType.DIRECT_COMPUTATION))
+
+    # assert all([v <= 3 for v in head_parents.values()])
+
+    for (child_name, child_index), count in head_parents.items():
+        if count == 3:
+            nodes_to_mask.append(TLACDCInterpNode(child_name, child_index, EdgeType.ADDITION))
+
+    for node in nodes_to_mask + additional_nodes_to_mask:
+        # Mark edges where this is child as not present
+        rest2 = corr.edges[node.name][node.index]
+        for rest3 in rest2.values():
+            for edge in rest3.values():
+                edge.present = False
+
+        # Mark edges where this is parent as not present
+        for rest1 in corr.edges.values():
+            for rest2 in rest1.values():
+                try:
+                    rest2[node.name][node.index].present = False
+                except KeyError as e:
+                    print("Warning: key error in correspondence_from_mask", e)
+                    pass
+    return corr
 
 
 def log_plotly_bar_chart(x: List[str], y: List[float]) -> None:
